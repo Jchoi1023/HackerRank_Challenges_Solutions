2ff8406b84979a9757a27255c810edcdc6ddf9ba
Jchoi1023 <jihye1023@gmail.com>
2025-09-17 21:26:54 -0500

    my solutions

---
+++ SQL/Aggregation/Average Population.sql
@@ -0,0 +1,8 @@
+/*
+https://www.hackerrank.com/challenges/average-population/problem?isFullScreen=true
+*/
+
+select floor(avg(population)) from city;
+
+-- rounded down = floor
+-- rounded up = ceiling
